73a20fc20c61b87f65efb1d2811d07aaea8c6856
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-12-10 08:03:56 +0000

    Creating feature test for impactor can see a list of posts.

---
+++ milkywire-client/cypress/integration/impactorCanSeePostsList.spec.js
@@ -0,0 +1,30 @@
+describe('Impactor can view post list', () => {
+  beforeEach(() => {
+    cy.server()
+  })
+  
+  it('successfully', () => {
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3001',
+      response: 'fixture:posts.json'
+    })
+    cy.visit('http://localhost:3001')
+  })
+  
+  it("contains post input filled", () => {
+    cy.get('#list h1')
+    .first().should('have.filled', 'Ocean Cleaning')
+    .next().should('have.filled', 'Donate and make your impact')
+  })
+    
+  it('sees message for no post', () => {
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3001',
+      response: '{"posts":[]}'
+    })
+    cy.visit('http://localhost:3001')
+    cy.get("#message").should('contain', 'There are no posts')
+    })
+  })
