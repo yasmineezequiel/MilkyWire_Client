e1af0cd0f9f0248eadd155d4fb247ee682049e94
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-12-13 16:26:18 +0100

    Adding feature test sad path.

--- milkywire-client/cypress/integration/impactorCanViewListedArticles.spec.js
+++ milkywire-client/cypress/integration/impactorCanViewListedArticles.spec.js
@@ -1,17 +1,29 @@
 describe('impactor can view listed posts', () => {
-it('sees posts displayed', () => {
-  cy.server()
-  cy.route({
-    method: 'GET',
-    url: 'https://api.example.com/posts',
-    response: 'fixture:posts.json'
-})
-  cy.visit('http://localhost:3001')
-  cy.get('h1').should('contain', 'Milkywire')
-  cy.get('h2')
-    .should('contain', 'Ocean Cleaning')
-    .should('contain', 'Milkywire')
-  cy.contains('Donate and make your impact')
-  cy.contains('Impactor report')
+  it('sees posts displayed', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'https://api.example.com/posts',
+      response: 'fixture:posts.json'
+  })
+    cy.visit('http://localhost:3001')
+    cy.get('h1').should('contain', 'Milkywire')
+    cy.get('h2')
+      .should('contain', 'Ocean Cleaning')
+      .should('contain', 'Milkywire')
+    cy.contains('Donate and make your impact')
+    cy.contains('Impactor report')
+    })
+
+  it('sees error message for no posts', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'https://api.example.com/posts',
+      response: 'fixture:error2.json',
+      status: 400
+    })
+    cy.visit('http://localhost:3001')
+    cy.contains('Network Error')
   })
 })
